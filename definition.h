3967c7f741e3ed4ebb29ecc5a1f5bbc55b8f3dea
cxcnearby <cxcnearby@126.com>
2019-03-22 00:46:20 +0800

    formatted

--- definition.h
+++ definition.h
@@ -1,20 +1,19 @@
 /***********************************************************
-*   Copyright (c) 2015-  Company Name.
-*   All rights reserved.
-*
-*   CODE NAME : Prometheus
-*   filename : definition.cc
-*   description :
-*
-*   current version : 1.0
-*   author : Wei Liu
-*   email : liuwei@ihep.ac.cn
-*   date : 2019.03.16
-*
-************************************************************/
+ *   Copyright (c) 2015-  Company Name.
+ *   All rights reserved.
+ *
+ *   CODE NAME : Prometheus
+ *   filename : definition.cc
+ *   description :
+ *
+ *   current version : 1.0
+ *   author : Wei Liu
+ *   email : liuwei@ihep.ac.cn
+ *   date : 2019.03.16
+ *
+ ************************************************************/
 
-
-struct big{
+struct big {
   double pmtx, pmty, ns, time;
   int eve, nhit, anode, dynode, fee, ch, sec;
 };
