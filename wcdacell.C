afcd75e9e400e1b6a90a097109fce98f3d0f3369
cxcnearby <cxcnearby@126.com>
2019-04-09 11:56:44 +0800

    add parameter output

--- wcdacell.C
+++ wcdacell.C
@@ -1,7 +1,13 @@
 {
+  FILE *fp_log;
   TChain *fChain; //! pointer to the analyzed TTree or TChain
   fChain = new TChain("tmatch");
-  fChain->Add("mcomb07.root");
+  fChain->Add("mcomb00.root");
+
+  if ((fp_log = fopen("fitpars.txt", "w+")) == NULL) {
+    printf("cannot create log file\n");
+    exit(0);
+  }
   // Fixed size dimensions of array or collections stored in the TTree if any.
 
   // Declaration of leaf types
@@ -35,96 +41,99 @@
   Long64_t time_df_dynode;
 
   // List of branches
-  TBranch *b_b_entry_big;        //!
-  TBranch *b_b_tot;              //!
-  TBranch *b_b_igcell;           //!
-  TBranch *b_b_fee_big;          //!
-  TBranch *b_b_ch;               //!
-  TBranch *b_b_x;                //!
-  TBranch *b_b_y;                //!
-  TBranch *b_b_anode_big;        //!
-  TBranch *b_b_dynode_big;       //!
-  TBranch *b_b_npe_big;          //!
-  TBranch *b_b_time_big;         //!
-  TBranch *b_b_deltatime_big;    //!
-  TBranch *b_b_entry_small;      //!
-  TBranch *b_b_fee_small;        //!
-  TBranch *b_b_db;               //!
-  TBranch *b_b_pmt;              //!
-  TBranch *b_b_anode_small;      //!
-  TBranch *b_b_dynode_small;     //!
-  TBranch *b_b_npe_small;        //!
-  TBranch *b_b_time_small;       //!
-  TBranch *b_b_time_anode;       //!
-  TBranch *b_b_time_dynode;      //!
-  TBranch *b_b_deltatime_small;  //!
-  TBranch *b_b_deltatime_anode;  //!
-  TBranch *b_b_deltatime_dynode; //!
-  /* TBranch        *b_b_time_s - b_time_b ;   //!
-     TBranch        *b_b_time_anode - b_time_b ;   //!
-     TBranch        *b_b_time_dynode - b_time_b ;   //!
-  */
+  TBranch *b_entry_big;        //!
+  TBranch *b_tot;              //!
+  TBranch *b_igcell;           //!
+  TBranch *b_fee_big;          //!
+  TBranch *b_ch;               //!
+  TBranch *b_x;                //!
+  TBranch *b_y;                //!
+  TBranch *b_anode_big;        //!
+  TBranch *b_dynode_big;       //!
+  TBranch *b_npe_big;          //!
+  TBranch *b_time_big;         //!
+  TBranch *b_deltatime_big;    //!
+  TBranch *b_entry_small;      //!
+  TBranch *b_fee_small;        //!
+  TBranch *b_db;               //!
+  TBranch *b_pmt;              //!
+  TBranch *b_anode_small;      //!
+  TBranch *b_dynode_small;     //!
+  TBranch *b_npe_small;        //!
+  TBranch *b_time_small;       //!
+  TBranch *b_time_anode;       //!
+  TBranch *b_time_dynode;      //!
+  TBranch *b_deltatime_small;  //!
+  TBranch *b_deltatime_anode;  //!
+  TBranch *b_deltatime_dynode; //!
+  TBranch *b_time_df;          //!
+  TBranch *b_time_df_anode;    //!
+  TBranch *b_time_df_dynode;   //!
 
-  fChain->SetBranchAddress("entry_number_b", &entry_number_b, &b_b_entry_big);
-  fChain->SetBranchAddress("total_number", &total_number, &b_b_tot);
-  fChain->SetBranchAddress("igcell", &igcell, &b_b_igcell);
-  fChain->SetBranchAddress("fee_b", &fee_b, &b_b_fee_big);
-  fChain->SetBranchAddress("channel", &channel, &b_b_ch);
-  fChain->SetBranchAddress("X", &X, &b_b_x);
-  fChain->SetBranchAddress("Y", &Y, &b_b_y);
-  fChain->SetBranchAddress("anode_b", &anode_b, &b_b_anode_big);
-  fChain->SetBranchAddress("dynode_b", &dynode_b, &b_b_dynode_big);
-  fChain->SetBranchAddress("npe_b", &npe_b, &b_b_npe_big);
-  fChain->SetBranchAddress("time_b", &time_b, &b_b_time_big);
-  fChain->SetBranchAddress("deltatime_b", &deltatime_b, &b_b_deltatime_big);
-  fChain->SetBranchAddress("entry_number_s", &entry_number_s, &b_b_entry_small);
-  fChain->SetBranchAddress("fee_s", &fee_s, &b_b_fee_small);
-  fChain->SetBranchAddress("db", &db, &b_b_db);
-  fChain->SetBranchAddress("pmt", &pmt, &b_b_pmt);
-  fChain->SetBranchAddress("anode_s", &anode_s, &b_b_anode_small);
-  fChain->SetBranchAddress("dynode_s", &dynode_s, &b_b_dynode_small);
-  fChain->SetBranchAddress("npe_s", &npe_s, &b_b_npe_small);
-  fChain->SetBranchAddress("time_s", &time_s, &b_b_time_small);
-  fChain->SetBranchAddress("anodetime_s", &anodetime_s, &b_b_time_anode);
-  fChain->SetBranchAddress("dynodetime_s", &dynodetime_s, &b_b_time_dynode);
-  fChain->SetBranchAddress("deltatime_s", &deltatime_s, &b_b_deltatime_small);
+  fChain->SetBranchAddress("entry_number_b", &entry_number_b, &b_entry_big);
+  fChain->SetBranchAddress("total_number", &total_number, &b_tot);
+  fChain->SetBranchAddress("igcell", &igcell, &b_igcell);
+  fChain->SetBranchAddress("fee_b", &fee_b, &b_fee_big);
+  fChain->SetBranchAddress("channel", &channel, &b_ch);
+  fChain->SetBranchAddress("X", &X, &b_x);
+  fChain->SetBranchAddress("Y", &Y, &b_y);
+  fChain->SetBranchAddress("anode_b", &anode_b, &b_anode_big);
+  fChain->SetBranchAddress("dynode_b", &dynode_b, &b_dynode_big);
+  fChain->SetBranchAddress("npe_b", &npe_b, &b_npe_big);
+  fChain->SetBranchAddress("time_b", &time_b, &b_time_big);
+  fChain->SetBranchAddress("deltatime_b", &deltatime_b, &b_deltatime_big);
+  fChain->SetBranchAddress("entry_number_s", &entry_number_s, &b_entry_small);
+  fChain->SetBranchAddress("fee_s", &fee_s, &b_fee_small);
+  fChain->SetBranchAddress("db", &db, &b_db);
+  fChain->SetBranchAddress("pmt", &pmt, &b_pmt);
+  fChain->SetBranchAddress("anode_s", &anode_s, &b_anode_small);
+  fChain->SetBranchAddress("dynode_s", &dynode_s, &b_dynode_small);
+  fChain->SetBranchAddress("npe_s", &npe_s, &b_npe_small);
+  fChain->SetBranchAddress("time_s", &time_s, &b_time_small);
+  fChain->SetBranchAddress("anodetime_s", &anodetime_s, &b_time_anode);
+  fChain->SetBranchAddress("dynodetime_s", &dynodetime_s, &b_time_dynode);
+  fChain->SetBranchAddress("deltatime_s", &deltatime_s, &b_deltatime_small);
   fChain->SetBranchAddress("deltatime_anode", &deltatime_anode,
-                           &b_b_deltatime_anode);
+                           &b_deltatime_anode);
   fChain->SetBranchAddress("deltatime_dynode", &deltatime_dynode,
-                           &b_b_deltatime_dynode);
+                           &b_deltatime_dynode);
 
-  fChain->SetBranchAddress("time_df", &time_df);
-  fChain->SetBranchAddress("time_df_anode", &time_df_anode);
-  fChain->SetBranchAddress("time_df_dynode", &time_df_dynode);
+  fChain->SetBranchAddress("time_df", &time_df, &b_time_df);
+  fChain->SetBranchAddress("time_df_anode", &time_df_anode, &b_time_df_anode);
+  fChain->SetBranchAddress("time_df_dynode", &time_df_dynode,
+                           &b_time_df_dynode);
 
   char buf1[500];
   char buf2[500];
   char buf3[500];
 
   TH1F *hh1[900];
-  TH1F *hh2[900];
-  TH1F *hh3[900];
-  TH1F *hh4[900];
-  int cellend = 2;
+  // TH1F *hh2[900];
+  // TH1F *hh3[900];
+  // TH1F *hh4[900];
+  int cellend = 900;
 
   TF1 *f1 = new TF1("f1", "gaus");
   f1->SetLineColor(kRed);
 
   double rms1[900];
-  double rms2[900];
-  double rms3[900];
-  double rms4[900];
+  // double rms2[900];
+  // double rms3[900];
+  // double rms4[900];
   double mean1[900];
-  double mean2[900];
-  double mean3[900];
-  double mean4[900];
+  // double mean2[900];
+  // double mean3[900];
+  // double mean4[900];
+
+  double par[3];
+  double parerr[3];
 
   TCanvas *c1 = new TCanvas("c1", "c1", 1500, 700);
-  c1->Divide(2, 2);
+  // c1->Divide(2, 2);
   for (int icell = 0; icell < cellend; icell++) {
     c1->Update();
 
-    c1->cd(1);
+    // c1->cd(1);
     sprintf(buf1, "time_df_anode>>h1_%d", icell);
 
     sprintf(buf2, "igcell==%d&&anode_s>0&&dynode_b>100", icell);
@@ -138,8 +147,15 @@
     rms1[icell] = hh1[icell]->GetRMS();
     hh1[icell]->Fit("f1", "", "", mean1[icell] - 2 * rms1[icell],
                     mean1[icell] + 2 * rms1[icell]);
+    f1->GetParameters(&par[0]);
+    parerr[0]=f1->GetParError(0);
+    parerr[1]=f1->GetParError(1);
+    parerr[2]=f1->GetParError(2);
 
-    sprintf(buf1, "pic/cell_%03d.pdf", icell);
+    fprintf(fp_log,"%9.3f\t%9.3f\t%9.3f\t%9.3f\t%9.3f\t%9.3f\n",par[0],parerr[0],par[1],parerr[1],par[2],parerr[2]);
+
+    sprintf(buf1, "pic/cell_%03d.png", icell);
     c1->SaveAs(buf1);
   }
+  fclose(fp_log);
 }
